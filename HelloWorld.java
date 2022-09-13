ecaa200b43309ea356c6c964d2ebb1311db011e4
wuhao <375564425@qq.com>
2022-09-13 19:42:48 +0800

    第二次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 		System.out.println("hello,world!");
 		System.out.println("hello,world!");
 		System.out.println("hello,world!");
+		System.out.println("hello,world!");
+		System.out.println("hello,world!");
     }
 }
